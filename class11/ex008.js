a15ef6ed6bd48768451ef69c141299f6f0601ae7
Amanda <amandabaratadev@gmail.com>
2022-12-15 16:14:43 +0000

    Ex008 simple condition

---
+++ class11/ex008.js
@@ -0,0 +1,10 @@
+var speed = 60.1
+console.log(`The speed of your car is ${speed} Km/h`)
+
+if(speed > 60) { // condicao simples!
+    console.log(`You overcome the speed limit, FINED!`)
+}
+
+console.log (`Always drive using the safety belt`)
+
+// para rodar o codigo no VS eu instalei o code runner e para ver basta clicar  control option N ou clicar  com o botão direito dentro do Vs code e clicar em  "code run" //
