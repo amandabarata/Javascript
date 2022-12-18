2c586e861669477e62df90867a6bfb000ea1bbb6
Amanda <amandabaratadev@gmail.com>
2022-12-18 23:19:10 +0000

    Class14 Loop for e depuração

---
+++ class14/ambiente.js
@@ -0,0 +1,24 @@
+/*
+var contador = 1
+while (contador <= 6) {
+    console.log(`Executar tarefa ${contador} vezes`)
+    contador ++
+}
+
+/*let i = 1
+while (i <= 6)  {
+ console.log(`Executar tarefa ${i} vezes`)
+ i++
+}*/
+
+/* estrutura:
+for (let i = 1; i <= 6; i++) {
+console.log(`Executar tarefa ${i} vezes`)
+}*/
+
+console.log('Vai começar...')
+for(let i = 1; i <= 4; i++) {
+    console.log(i)
+}
+console.log('FIM')
+
