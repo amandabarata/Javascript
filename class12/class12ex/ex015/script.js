bacbdfbd0eacd1425719b83a9969520cbbff5f18
Amanda <amandabaratadev@gmail.com>
2022-12-18 20:51:57 +0000

    ex015 finished age verifyer

--- class12/class12ex/ex015/script.js
+++ class12/class12ex/ex015/script.js
@@ -1,13 +1,52 @@
+
 function verify() {
     var data = new Date()
     var year = data.getFullYear()
-    var formyear = document.getElementById('txtyear')
+    var fano = document.getElementById('txtyear')
     var res = document.querySelector('div#res')
-    if(formyear.value.length == 0 || formyear.value > year) {
+    
+    if(fano.value.length == 0 || fano.value > year )   {
         alert('[ERROR] check the data again!')
     }
+    else {
+    var fgender = document.getElementsByName('gender')
+    var age = year - Number(fano.value)
+    var genero = ''
+    var img = document.createElement('img')
+    img.setAttribute('id' , 'foto')
+    if (fgender[0].checked) {
+        genero = 'man'
+        if (age >= 0 && age < 10) {
+            //Child
+            img.setAttribute ('src', 'foto-bebe-m.png')
+        } else if (age < 21) {
+            //Young
+            img.setAttribute ('src', 'foto-adulto-m.png')
+        } else if ( age < 50) {
+            //Adult
+            img.setAttribute ('src', 'foto-adulto-m.png')
+        } else {
+            //Old
+            img.setAttribute ('src', 'foto-idoso-m.png')
+        }
+    } else if (fgender[1].checked) {
+        genero = 'woman'
+        if (age >=0 && age < 10) {
+            //Child
+            img.setAttribute ('src', 'foto-bebe-girl.png')
+        } else if (age < 21) {
+            //Young
+            img.setAttribute ('src', 'foto-adulto-mulher.png')
+        } else if ( age < 50) {
+            //Adult
+            img.setAttribute ('src', 'foto-adulto-mulher.png')
+        } else {
+            //Old
+            img.setAttribute ('src', 'foto-idosa-m.png')
+        }
+    }
+    res.style.textAlign = 'center'
+    res.innerHTML = `We detected a ${genero} with ${age} years old.`
+    res.appendChild(img)
 }
-
-else {
-    alert('Everything is ok')
 }
