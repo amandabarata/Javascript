e49d509472a4edcb897d3024ca25c56ed1d1bb3a
Amanda <amandabaratadev@gmail.com>
2022-12-18 13:47:53 +0000

    made js, but is not working

--- class12/class12ex/ex015/script.js
+++ class12/class12ex/ex015/script.js
@@ -1,1 +1,13 @@
-alert('Heyyy!')
+function verify() {
+    var data = new Date()
+    var year = data.getFullYear()
+    var formyear = document.getElementById('txtyear')
+    var res = document.querySelector('div#res')
+    if(formyear.value.length == 0 || formyear.value > year) {
+        alert('[ERROR] check the data again!')
+    }
+}
+
+else {
+    alert('Everything is ok')
+}
