e301c64853c7aa101c2162d0d429017ee78b5b8f
Amanda <amandabaratadev@gmail.com>
2022-12-18 22:31:14 +0000

    Class 13 - Loops while e do while

---
+++ class13/ambiente.js
@@ -0,0 +1,27 @@
+/* teste lógico no início (mais comum) =
+enquanto(condição  for  verdadeira) faça)*/
+var contador = 1
+while (contador <= 6) {
+    console.log(`Passo ${contador} vezes`)
+    contador++
+}
+
+
+/* Teste lógico no final (faça x enquanto(condição))
+var contador = 1
+do {
+    console.log(`Passo ${contador} vezes`)
+    contador++
+} while (contador <= 6)
+*/
+
+
+
+/*console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')*/
+
+
