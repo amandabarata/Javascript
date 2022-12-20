6db4c300088d7e31ba172880cc32941810ccf97b
Amanda <amandabaratadev@gmail.com>
2022-12-20 21:35:36 +0000

    created class15

---
+++ class15/ambiente.js
@@ -0,0 +1,6 @@
+
+let num = [5, 8, 2, 9, 3]
+num.sort()
+num.push(1)
+console.log(`My array tem ${num.length} position`)
+console.log(` valor da array é ${num[0]}`)
---
+++ class15/arrayonthescreen.js
@@ -0,0 +1,13 @@
+let num = [8, 1, 7, 4, 2, 9]
+
+/*for (let pos = 0; pos < valores.length; pos++) {
+    console.log(`The position ${pos} tem o valor de ${valores[pos]}`)
+}*/
+
+ /*Forma mais simples for in:*/
+for(let pos in num) {
+    console.log(`The position ${pos} tem o valor de ${num[pos]}`)
+}
+
+
+
